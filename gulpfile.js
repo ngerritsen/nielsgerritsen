bbda24074c39076187d63d3d41a1287894dfa011
Aesthetickz <n.c.gerritsen@gmail.com>
2015-08-05 21:20:09 +0200

    remove imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -75,11 +75,11 @@ gulp.task('minify-js', function() {
 
 gulp.task('images', function () {
     return gulp.src('app/images/**/*')
-        .pipe($.cache($.imagemin({
-            optimizationLevel: 3,
-            progressive: true,
-            interlaced: true
-        })))
+        // .pipe($.cache($.imagemin({
+        //     optimizationLevel: 3,
+        //     progressive: true,
+        //     interlaced: true
+        // })))
         .pipe(gulp.dest('dist/images'))
         .pipe($.size());
 });
